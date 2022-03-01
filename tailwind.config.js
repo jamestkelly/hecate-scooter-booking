09405709344eaf1a130743b69fc074df86047e33
jamestkelly <jimkelly.t@outlook.com>
2022-03-02 09:04:50 +1100

    Finalised login component

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'theme-primary': '#073B3A',
+      'theme-secondary': '#0B6E4F',
+      'theme-tertiary': '#08A045',
+      'text-primary': '#6BBF59',
+      'text-secondary': '#DDB771',
+      'off-white': '#FAF9F6'
+    },
+    fontFamily: {
+      'roboto': ["'Roboto'", 'sans-serif']
+    }
   },
   plugins: [],
 }
